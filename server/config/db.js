10278db86cba4296f1cb819e4b1c32537b5f2d51
Pattabi Raman <ramanpattabi843@gmail.com>
2025-09-08 10:20:18 +0530

    Add db.js with Render PostgreSQL configuration

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,22 @@
+
+// db.js
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  "student_management", // DB name
-  "postgres",           // DB user
-  "pattabi",      // DB password
+  "student_management_cp3y", // Database name
+  "student_management_cp3y_user", // Username
+  "RRFv1BHKxybWdrm7wK5TDfm7qL0WcimN", // 🔑 Replace with your Render DB password
   {
-    host: "localhost",
+    host: "dpg-d2v4ckp5pdvs73b1na5g-a.singapore-postgres.render.com", // External hostname
+    port: 5432,
     dialect: "postgres",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Render requires SSL
+      },
+    },
   }
 );
 
